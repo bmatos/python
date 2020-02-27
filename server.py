5a0b8b8c424ec0af68ff134be3a23c111fa52055
BMatos <bmatos@fusionstorm.com>
2020-02-27 16:52:50 -0500

    new python web server

--- server.py
+++ server.py
@@ -6,5 +6,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("",PORT), Handler) as httpd:
     print("Server is running at ", PORT)
-    Print ("To test from dockerized version, please run http://loaclhost:<exposed port>")
+    print("To test from dockerized version, please run http://loaclhost:<exposed port>")
     httpd.serve_forever()
